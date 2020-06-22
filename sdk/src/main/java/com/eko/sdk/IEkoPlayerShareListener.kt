875dbd19722b0498c089e96de5ef601193618ab5
Elad Gil <elad@helleko.com>
2020-06-22 19:11:09 +0300

    updated javadoc

--- sdk/src/main/java/com/eko/sdk/IEkoPlayerShareListener.kt
+++ sdk/src/main/java/com/eko/sdk/IEkoPlayerShareListener.kt
@@ -1,5 +1,12 @@
 package com.eko.sdk
 
+/**
+ * Listener interface for share events.
+ */
 interface IEkoPlayerShareListener {
+    /**
+     * There can be share intents from within an eko project via share buttons or ekoshell. This function will be called whenever a share intent happened.
+     * @param url The canonical url of the project.
+     */
     fun onShare(url: String)
 }
--- sdk/src/main/java/com/eko/sdk/IEkoPlayerUrlListener.kt
+++ sdk/src/main/java/com/eko/sdk/IEkoPlayerUrlListener.kt
@@ -8,6 +8,7 @@ interface IEkoPlayerUrlListener {
      * There can be link outs from within an eko video.
      * This function will be called whenever a link out is supposed to occur.
      * The listener is responsible for opening the url.
+     * @param url The url to open.
      */
     fun onUrlOpen(url: String)
 }
