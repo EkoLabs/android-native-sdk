a080ca1b19e9383c31d5ad407d7b14a75cf607f8
Elad Gil <elad@helleko.com>
2020-06-29 14:57:38 +0300

    added try/catch to the JS message parsing just to be on the safe side

--- sdk/src/main/java/com/eko/sdk/JSBridge.kt
+++ sdk/src/main/java/com/eko/sdk/JSBridge.kt
@@ -2,6 +2,7 @@ package com.eko.sdk
 
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.webkit.JavascriptInterface
 import org.json.JSONArray
 import org.json.JSONObject
@@ -9,9 +10,13 @@ import org.json.JSONObject
 class JSBridge(private var onEvent: (String, JSONArray?) -> Unit) {
     @JavascriptInterface
     fun postMessage(data: String) {
-        val dataObj = JSONObject(data)
-        Handler(Looper.getMainLooper()).post { ->
-            onEvent(dataObj.getString("type"), dataObj.optJSONArray("args"))
+        try {
+            val dataObj = JSONObject(data)
+            Handler(Looper.getMainLooper()).post { ->
+                onEvent(dataObj.getString("type"), dataObj.optJSONArray("args"))
+            }
+        } catch (error: Error) {
+            Log.e("EkoPlayer", "Failed to parse message from project", error)
         }
     }
 }
